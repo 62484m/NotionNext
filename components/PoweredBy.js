9b71edc4326f156256fd86330370d213681a5e3c
机长测评 <mh519@vip.qq.com>
2025-02-11 19:07:21 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -11,8 +11,7 @@ export default function PoweredBy(props) {
       <a
         // href='https://github.com/tangly1024/NotionNext'
         className='underline justify-start'>
-        // NotionNext {siteConfig('VERSION')}
-          N
+          No.
       </a>
       .
     </div>
