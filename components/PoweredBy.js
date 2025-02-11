50d98ac0f8789f6d986575b79b1077578e4ae3ad
机长测评 <mh519@vip.qq.com>
2025-02-11 19:04:48 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,9 +9,10 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        // href='https://github.com/tangly1024/NotionNext'
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        // NotionNext {siteConfig('VERSION')}
+          N
       </a>
       .
     </div>
